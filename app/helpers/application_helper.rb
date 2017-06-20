e469efc4cd2aa185e24cb2e79558b462e0c01e1d
CoycLab <coyc@inbox.ru>
2017-06-20 20:56:12 +0300

    Added the ability to select a user using CheckBox

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def user_full_name
+    "#{@project.user.first_name} #{@project.user.last_name}"
+  end
+
+  def logged_user_full_name
+    "#{current_user.first_name} #{current_user.last_name}"
+  end
 end
